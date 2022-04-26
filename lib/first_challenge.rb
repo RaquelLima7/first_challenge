d472297fbb510e7a0869cded7e917a7475003565
RaquelLima7 <raquellimadev@gmail.com>
2022-04-25 22:19:12 -0300

    new exemples

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -33,3 +33,189 @@ def example_three(array_string)
     end
   end
 end
+
+def example_four(array_string)
+  array_string.map do |arr|
+    if arr.include?('B.GA.2')
+      arr.sub('B.GA.2', 'B.GA.1')
+    elsif arr.include?('B.GA.3')
+      arr.sub('B.GA.3', 'B.GA.2')
+    elsif arr.include?('B.GA.4')
+      arr.sub('B.GA.4', 'B.GA.3')
+    elsif arr.include?('B.OG.1.1')
+      arr.sub('B.OG.1.1', 'B.GA.4')
+    elsif arr.include?('B.OG.1.2')
+      arr.sub('B.OG.1.2', 'B.GA.5')
+    elsif arr.include?('B.OG.1.3')
+      arr.sub('B.OG.1.3', 'B.OG.1.1')
+    elsif arr.include?('B.OG.1.4')
+      arr.sub('B.OG.1.4', 'B.OG.1.2')
+    elsif arr.include?('B.OG.1.5')
+      arr.sub('B.OG.1.5', 'B.OG.1.3')
+    elsif arr.include?('B.OG.2.1')
+      arr.sub('B.OG.2.1', 'B.OG.1.4')
+    else
+      arr
+    end
+  end
+end
+
+def example_five(array_string)
+  array_string.map do |arr|
+    if arr.include?('B.OG.2.3')
+      arr.sub('B.OG.2.3', 'B.OG.1.5')
+    elsif arr.include?('B.OG.2.4')
+      arr.sub('B.OG.2.4', 'B.OG.2.1')
+    elsif arr.include?('B.OG.2.5')
+      arr.sub('B.OG.2.5', 'B.OG.2.2')
+    elsif arr.include?('B.PH.1')
+      arr.sub('B.PH.1', 'B.OG.2.3')
+    elsif arr.include?('B.PH.2')
+      arr.sub('B.PH.2', 'B.OG.2.4')
+    elsif arr.include?('B.GA.5')
+      arr.sub('B.GA.5', 'B.OG.2.5')
+    elsif arr.include?('B.GA.1')
+      arr.sub('B.GA.1', 'B.PH.1')
+    elsif arr.include?('B.OG.2.2')
+      arr.sub('B.OG.2.2', 'B.PH.2')
+    else
+      arr
+    end
+  end
+end
+
+def example_six(array_string)
+  array_string.map do |arr|
+    if arr.include?('C.OG.2.2')
+      arr.sub('C.OG.2.2', 'C.GA.1')
+    elsif arr.include?('C.GA.1')
+      arr.sub('C.GA.1', 'C.GA.2')
+    elsif arr.include?('C.GA.2')
+      arr.sub('C.GA.2', 'C.GA.3')
+    elsif arr.include?('C.GA.3')
+      arr.sub('C.GA.3', 'C.GA.4')
+    elsif arr.include?('C.GA.4')
+      arr.sub('C.GA.4', 'C.GA.5')
+    elsif arr.include?('C.GA.5')
+      arr.sub('C.GA.5', 'C.OG.1.1')
+    elsif arr.include?('C.OG.1.1')
+      arr.sub('C.OG.1.1', 'C.OG.1.2')
+    elsif arr.include?('C.OG.1.2')
+      arr.sub('C.OG.1.2', 'C.OG.1.3')
+    elsif arr.include?('C.OG.1.3')
+      arr.sub('C.OG.1.3', 'C.OG.1.4')
+    elsif arr.include?('C.OG.1.4')
+      arr.sub('C.OG.1.4', 'C.OG.1.5')
+    elsif arr.include?('C.OG.1.5')
+      arr.sub('C.OG.1.5', 'C.OG.2.1')
+    else
+      arr
+    end
+  end
+end
+
+def example_seven(array_string)
+  array_string.map do |arr|
+    if arr.include?('C.OG.2.1')
+      arr.sub('C.OG.2.1', 'C.OG.2.2')
+    elsif arr.include?('D.OG.2.1')
+      arr.sub('D.OG.2.1', 'D.GA.1')
+    elsif arr.include?('D.GA.1')
+      arr.sub('D.GA.1', 'D.GA.2')
+    elsif arr.include?('D.GA.2')
+      arr.sub('D.GA.2', 'D.GA.3')
+    elsif arr.include?('D.GA.3')
+      arr.sub('D.GA.3', 'D.OG.1.1')
+    elsif arr.include?('D.OG.1.1')
+      arr.sub('D.OG.1.1', 'D.OG.1.2')
+    elsif arr.include?('D.OG.1.2')
+      arr.sub('D.OG.1.2', 'D.OG.1.3')
+    elsif arr.include?('D.OG.1.3')
+      arr.sub('D.OG.1.3', 'D.OG.1.4')
+    elsif arr.include?('D.OG.1.4')
+      arr.sub('D.OG.1.4', 'D.OG.2.1')
+    else
+      arr
+    end
+  end
+end
+
+def example_eight(array_string)
+  array_string.map do |arr|
+    if arr.include?('F.PH.1')
+      arr.sub('F.PH.1', 'F.OG.2.5')
+    elsif arr.include?('F.PH.2')
+      arr.sub('F.PH.2', 'F.PH.1')
+    elsif arr.include?('F.OG.2.5')
+      arr.sub('F.OG.2.5', 'F.PH.2')
+    elsif arr.include?('G.OG.1.2')
+      arr.sub('G.OG.1.2', 'G.GA.1')
+    elsif arr.include?('G.GA.1')
+      arr.sub('G.GA.1', 'G.GA.2')
+    elsif arr.include?('G.GA.2')
+      arr.sub('G.GA.2', 'G.GA.3')
+    elsif arr.include?('G.GA.3')
+      arr.sub('G.GA.3', 'G.GA.4')
+    elsif arr.include?('G.GA.4')
+      arr.sub('G.GA.4', 'G.GA.5')
+    elsif arr.include?('G.GA.5')
+      arr.sub('G.GA.5', 'G.OG.1.1')
+    else
+      arr
+    end
+  end
+end
+
+def example_nine(array_string)
+  array_string.map do |arr|
+    if arr.include?('G.OG.1.1')
+      arr.sub('G.OG.1.1', 'G.OG.1.2')
+    elsif arr.include?('G.OG.1.4')
+      arr.sub('G.OG.1.4', 'G.OG.1.3')
+    elsif arr.include?('G.OG.1.5')
+      arr.sub('G.OG.1.5', 'G.OG.1.4')
+    elsif arr.include?('G.OG.2.1')
+      arr.sub('G.OG.2.1', 'G.OG.1.5')
+    elsif arr.include?('G.OG.2.2')
+      arr.sub('G.OG.2.2', 'G.OG.2.1')
+    elsif arr.include?('G.OG.2.3')
+      arr.sub('G.OG.2.3', 'G.OG.2.2')
+    elsif arr.include?('G.OG.2.4')
+      arr.sub('G.OG.2.4', 'G.OG.2.3')
+    elsif arr.include?('G.OG.2.5')
+      arr.sub('G.OG.2.5', 'G.OG.2.4')
+    elsif arr.include?('G.PH.1')
+      arr.sub('G.PH.1', 'G.OG.2.5')
+    elsif arr.include?('G.PH.2')
+      arr.sub('G.PH.2', 'G.PH.1')
+    elsif arr.include?('G.OG.1.3')
+      arr.sub('G.OG.1.3', 'G.PH.2')
+    else
+      arr
+    end
+  end
+end
+
+def example_ten(array_string)
+  array_string.map do |arr|
+    if arr.include?('H.OG.2.1')
+      arr.sub('H.OG.2.1', 'H.OG.1.6')
+    elsif arr.include?('H.OG.2.2')
+      arr.sub('H.OG.2.2', 'H.OG.2.1')
+    elsif arr.include?('H.OG.2.3')
+      arr.sub('H.OG.2.3', 'H.OG.2.2')
+    elsif arr.include?('H.OG.2.4')
+      arr.sub('H.OG.2.4', 'H.OG.2.3')
+    elsif arr.include?('H.OG.2.5')
+      arr.sub('H.OG.2.5', 'H.OG.2.4')
+    elsif arr.include?('H.OG.2.6')
+      arr.sub('H.OG.2.6', 'H.OG.2.5')
+    elsif arr.include?('H.PH.1')
+      arr.sub('H.PH.1', 'H.OG.2.6')
+    elsif arr.include?('H.OG.1.6')
+      arr.sub('H.OG.1.6', 'H.PH.1')
+    else
+      arr
+    end
+  end
+end
--- spec/first_challenge_spec.rb
+++ spec/first_challenge_spec.rb
@@ -20,3 +20,60 @@ describe '#example_three' do
                           'BA1 / 1.10.a'])).to match ['BA1 / 1.1.x', 'BA1 / 1.10.a', 'BA1 / 1.10.c']
   end
 end
+
+describe '#example_four' do
+  it 'should replace the characters - example_four' do
+    expect(example_four(['A / A.GA.1', 'A / A.GA.2', 'A / A.GA.3', 'A / A.GA.4', 'A / A.GA.5', 'A / A.GA.6', 'A / A.OG.1.1', 'A / A.OG.1.2', 'A / A.OG.1.3', 'A / A.OG.1.4', 'A / A.OG.1.5', 'A / A.OG.1.6', 'A / A.OG.2.1', 'A / A.OG.2.2', 'A / A.OG.2.3', 'A / A.OG.2.4', 'A / A.OG.2.5', 'A / A.OG.2.6', 'A / A.PH.1', 'A / A.PH.2', 'A / A.PH.3', 'B / B.GA.2', 'B / B.GA.3', 'B / B.GA.4', 'B / B.OG.1.1', 'B / B.OG.1.2', 'B / B.OG.1.3', 'B / B.OG.1.4', 'B / B.OG.1.5', 'B / B.OG.2.1'])).to match [
+      'A / A.GA.1', 'A / A.GA.2', 'A / A.GA.3', 'A / A.GA.4', 'A / A.GA.5', 'A / A.GA.6', 'A / A.OG.1.1', 'A / A.OG.1.2', 'A / A.OG.1.3', 'A / A.OG.1.4', 'A / A.OG.1.5', 'A / A.OG.1.6', 'A / A.OG.2.1', 'A / A.OG.2.2', 'A / A.OG.2.3', 'A / A.OG.2.4', 'A / A.OG.2.5', 'A / A.OG.2.6', 'A / A.PH.1', 'A / A.PH.2', 'A / A.PH.3', 'B / B.GA.1', 'B / B.GA.2', 'B / B.GA.3', 'B / B.GA.4', 'B / B.GA.5', 'B / B.OG.1.1', 'B / B.OG.1.2', 'B / B.OG.1.3', 'B / B.OG.1.4'
+    ]
+  end
+end
+
+describe '#example_five' do
+  it 'should replace the characters - example_five' do
+    expect(example_five(['B / B.OG.2.3', 'B / B.OG.2.4', 'B / B.OG.2.5', 'B / B.PH.1', 'B / B.PH.2', 'B / B.GA.5', 'B / B.GA.1', 'B / B.OG.2.2'])).to match [
+      'B / B.OG.1.5', 'B / B.OG.2.1', 'B / B.OG.2.2', 'B / B.OG.2.3', 'B / B.OG.2.4', 'B / B.OG.2.5', 'B / B.PH.1', 'B / B.PH.2'
+    ]
+  end
+end
+
+describe '#example_six' do
+  it 'should replace the characters - example_six' do
+    expect(example_six(['C / C.OG.2.2', 'C / C.GA.1', 'C / C.GA.2', 'C / C.GA.3', 'C / C.GA.4', 'C / C.GA.5', 'C / C.OG.1.1', 'C / C.OG.1.2', 'C / C.OG.1.3', 'C / C.OG.1.4', 'C / C.OG.1.5'])).to match [
+      'C / C.GA.1', 'C / C.GA.2', 'C / C.GA.3', 'C / C.GA.4', 'C / C.GA.5', 'C / C.OG.1.1', 'C / C.OG.1.2', 'C / C.OG.1.3', 'C / C.OG.1.4', 'C / C.OG.1.5', 'C / C.OG.2.1'
+    ]
+  end
+end
+
+describe '#example_seven' do
+  it 'should replace the characters - example_seven' do
+    expect(example_seven(['C / C.OG.2.1', 'C / C.OG.2.3', 'C / C.OG.2.4', 'C / C.OG.2.5', 'C / C.PH.1', 'C / C.PH.2', 'D / D.OG.2.1', 'D / D.GA.1', 'D / D.GA.2', 'D / D.GA.3', 'D / D.OG.1.1', 'D / D.OG.1.2', 'D / D.OG.1.3', 'D / D.OG.1.4', 'D / D.OG.2.2', 'D / D.OG.2.3', 'D / D.OG.2.4', 'D / D.PH.1', 'D / D.PH.2'])).to match [
+      'C / C.OG.2.2', 'C / C.OG.2.3', 'C / C.OG.2.4', 'C / C.OG.2.5', 'C / C.PH.1', 'C / C.PH.2', 'D / D.GA.1', 'D / D.GA.2', 'D / D.GA.3', 'D / D.OG.1.1', 'D / D.OG.1.2', 'D / D.OG.1.3', 'D / D.OG.1.4', 'D / D.OG.2.1', 'D / D.OG.2.2', 'D / D.OG.2.3', 'D / D.OG.2.4', 'D / D.PH.1', 'D / D.PH.2'
+    ]
+  end
+end
+
+describe '#example_eight' do
+  it 'should replace the characters - example_eight' do
+    expect(example_eight(['E / E.GA.1', 'E / E.GA.2', 'E / E.GA.3', 'E / E.OG.1.1', 'E / E.OG.1.2', 'E / E.OG.1.3', 'E / E.OG.1.4', 'E / E.OG.2.1', 'E / E.OG.2.2', 'E / E.OG.2.3', 'E / E.OG.2.4', 'E / E.PH.1', 'E / E.PH.2', 'F / F.GA.1', 'F / F.GA.2', 'F / F.GA.3', 'F / F.GA.4', 'F / F.GA.5', 'F / F.OG.1.1', 'F / F.OG.1.2', 'F / F.OG.1.3', 'F / F.OG.1.4', 'F / F.OG.1.5', 'F / F.OG.2.1', 'F / F.OG.2.2', 'F / F.OG.2.3', 'F / F.OG.2.4', 'F / F.PH.1', 'F / F.PH.2', 'F / F.OG.2.5',
+                          'G / G.OG.1.2', 'G / G.GA.1', 'G / G.GA.2', 'G / G.GA.3', 'G / G.GA.4', 'G / G.GA.5'])).to match [
+                            'E / E.GA.1', 'E / E.GA.2', 'E / E.GA.3', 'E / E.OG.1.1', 'E / E.OG.1.2', 'E / E.OG.1.3', 'E / E.OG.1.4', 'E / E.OG.2.1', 'E / E.OG.2.2', 'E / E.OG.2.3', 'E / E.OG.2.4', 'E / E.PH.1', 'E / E.PH.2', 'F / F.GA.1', 'F / F.GA.2', 'F / F.GA.3', 'F / F.GA.4', 'F / F.GA.5', 'F / F.OG.1.1', 'F / F.OG.1.2', 'F / F.OG.1.3', 'F / F.OG.1.4', 'F / F.OG.1.5', 'F / F.OG.2.1', 'F / F.OG.2.2', 'F / F.OG.2.3', 'F / F.OG.2.4', 'F / F.OG.2.5', 'F / F.PH.1', 'F / F.PH.2', 'G / G.GA.1', 'G / G.GA.2', 'G / G.GA.3', 'G / G.GA.4', 'G / G.GA.5', 'G / G.OG.1.1'
+                          ]
+  end
+end
+
+describe '#example_nine' do
+  it 'should replace the characters - example_nine' do
+    expect(example_nine(['G / G.OG.1.1', 'G / G.OG.1.4', 'G / G.OG.1.5', 'G / G.OG.2.1', 'G / G.OG.2.2', 'G / G.OG.2.3', 'G / G.OG.2.4', 'G / G.OG.2.5', 'G / G.PH.1', 'G / G.PH.2', 'G / G.OG.1.3'])).to match [
+      'G / G.OG.1.2', 'G / G.OG.1.3', 'G / G.OG.1.4', 'G / G.OG.1.5', 'G / G.OG.2.1', 'G / G.OG.2.2', 'G / G.OG.2.3', 'G / G.OG.2.4', 'G / G.OG.2.5', 'G / G.PH.1', 'G / G.PH.2'
+    ]
+  end
+end
+
+describe '#example_ten' do
+  it 'should replace the characters - example_ten' do
+    expect(example_ten(['H / H.GA.1', 'H / H.GA.2', 'H / H.GA.3', 'H / H.GA.4', 'H / H.GA.5', 'H / H.GA.6', 'H / H.OG.1.1', 'H / H.OG.1.2', 'H / H.OG.1.3', 'H / H.OG.1.4', 'H / H.OG.1.5', 'H / H.OG.2.1', 'H / H.OG.2.2', 'H / H.OG.2.3', 'H / H.OG.2.4', 'H / H.OG.2.5', 'H / H.OG.2.6', 'H / H.PH.1', 'H / H.OG.1.6', 'H / H.PH.2', 'H / H.PH.3'])).to match [
+      'H / H.GA.1', 'H / H.GA.2', 'H / H.GA.3', 'H / H.GA.4', 'H / H.GA.5', 'H / H.GA.6', 'H / H.OG.1.1', 'H / H.OG.1.2', 'H / H.OG.1.3', 'H / H.OG.1.4', 'H / H.OG.1.5', 'H / H.OG.1.6', 'H / H.OG.2.1', 'H / H.OG.2.2', 'H / H.OG.2.3', 'H / H.OG.2.4', 'H / H.OG.2.5', 'H / H.OG.2.6', 'H / H.PH.1', 'H / H.PH.2', 'H / H.PH.3'
+    ]
+  end
+end
